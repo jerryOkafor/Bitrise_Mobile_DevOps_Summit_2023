faeb4f965522977227cc24c96b379ffe47549512
Jerry Okafor <jerryhanksokafor@gmail.com>
2023-09-19 20:14:47 -0600

    feat : update scripts

--- scripts/secrets.sh
+++ scripts/secrets.sh
@@ -5,7 +5,7 @@ set -ex
 echo 'Generating local.properties...'
 touch "./local.properties"
 
-echo "SAMPLE_APP_ENV_VAR=$SAMPLE_APP_ENV_VAR" >>local.properties
-echo "SAMPLE_APP_ENV_VAR_REPLACED=$SAMPLE_APP_ENV_VAR_REPLACED" >>local.properties
-echo "SAMPLE_SECRET=$SAMPLE_SECRET" >>local.properties
-echo "SAMPLE_SECRET_REPLACED=$SAMPLE_SECRET_REPLACED" >>local.properties
+envman run bash -c 'echo "SAMPLE_APP_ENV_VAR=$SAMPLE_APP_ENV_VAR" >>local.properties'
+envman run bash -c 'echo "SAMPLE_APP_ENV_VAR_REPLACED=$SAMPLE_APP_ENV_VAR_REPLACED" >>local.properties'
+envman run bash -c 'echo "SAMPLE_SECRET=$SAMPLE_SECRET" >>local.properties'
+envman run bash -c 'echo "SAMPLE_SECRET_REPLACED=$SAMPLE_SECRET_REPLACED" >>local.properties'
